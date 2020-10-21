87d02df2b87dd51d8e8fce1209bfaba65060661e
larzraz <lars5030@edu.ucl.dk>
2020-10-21 14:48:19 +0200

    Added functionality for toggle approval for workshops in frontend

--- AdminApp/Shared/Workshops/WorkshopModel.cs
+++ AdminApp/Shared/Workshops/WorkshopModel.cs
@@ -8,6 +8,8 @@ namespace AdminApp.Shared.Workshops
     public class WorkshopModel
     {
         public string Name { get; set; }
+        public string Approved { get; set; }
+        public Guid Id { get; set; }
         //public string Address { get; set; }
         //public string Zipcode { get; set; }
         //public string Url { get; set; }
